094457c160f4d2a9b3aae77dc10e88b7ac7166db
Dylan Frohberg <dmfrohberg@gmail.com>
2023-06-02 13:29:16 -0700

    Branch broke and had to delete local repo, clone remote, and copy over changed files

--- app/src/main/java/cse340/finalproject/MainActivity.java
+++ app/src/main/java/cse340/finalproject/MainActivity.java
@@ -2,7 +2,9 @@ package cse340.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.profile_button_main).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+        findViewById(R.id.settings_button_main).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 }
